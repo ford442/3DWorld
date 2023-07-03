daf3966f4b66d4df61916ad42f623f49c58f8462
Noah Cohn <noahc42@gmail.com>
2023-07-03 06:23:35 -0700

    Update gl_includes.h

--- src/gl_includes.h
+++ src/gl_includes.h
@@ -16,7 +16,7 @@
 #include <freeglut.h>
 #undef FAR // undefine conflicting defines picked up from windows headers
 #else // linux
-#include <GL/freeglut.h> // standard glut
+#include "usr/include/GL/freeglut.h" // standard glut
 #endif
 
 #include <GL/glu.h>
